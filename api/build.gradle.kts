3e3ea91a8c9bc4f82317be62c2a3d8c93389ef63
kahverengi <ahmet@duruer.dev>
2023-11-04 19:59:56 +0300

    Add api module

---
+++ api/build.gradle.kts
@@ -0,0 +1,18 @@
+val pf4jVersion: String by project
+
+plugins {
+    id("java")
+    kotlin("jvm") version "1.9.10"
+}
+
+group = "co.statu.parsek.api"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    compileOnly("org.pf4j:pf4j:${pf4jVersion}")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,3 @@
 rootProject.name = "Parsek"
 
+include("api")
