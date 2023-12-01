10da25eea32370a085f3211dc0f50863b32abd2d
kahverengi <ahmet@duruer.dev>
2023-12-02 02:31:57 +0300

    Add data and meta feature for response object

--- src/main/kotlin/co/statu/parsek/model/Successful.kt
+++ src/main/kotlin/co/statu/parsek/model/Successful.kt
@@ -2,11 +2,18 @@ package co.statu.parsek.model
 
 import co.statu.parsek.model.Result.Companion.encode
 
-open class Successful(val responseMap: Map<String, Any?> = mapOf()) : Result {
+open class Successful(private val data: Any? = null, private val meta: Map<String, Any?>? = null) : Result {
     override fun encode(extras: Map<String, Any?>): String {
         val response = mutableMapOf<String, Any?>()
 
-        response.putAll(responseMap)
+        if (data != null) {
+            response["data"] = data
+        }
+
+        if (meta != null) {
+            response["meta"] = meta
+        }
+
         response.putAll(extras)
 
         return response.encode()
