2146cbbdf6b5852e8926eaaf2b04c2c05a87ff5c
kahverengi <ahmet@duruer.dev>
2023-12-09 21:34:37 +0300

    Add convertMillisToISO8601 method to date util

--- src/main/kotlin/co/statu/parsek/util/DateUtil.kt
+++ src/main/kotlin/co/statu/parsek/util/DateUtil.kt
@@ -1,6 +1,8 @@
 package co.statu.parsek.util
 
 import java.time.Instant
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 import java.util.*
 
 object DateUtil {
@@ -12,6 +14,13 @@ object DateUtil {
         calendar[Calendar.MILLISECOND] = 0
 
         return calendar.timeInMillis
+    }
+
+    fun convertMillisToISO8601(millis: Long): String {
+        val instant = Instant.ofEpochMilli(millis)
+        val formatter = DateTimeFormatter.ISO_OFFSET_DATE_TIME
+            .withZone(ZoneOffset.UTC)
+        return formatter.format(instant)
     }
 
     fun convertISO8601ToMillis(iso8601Date: String) = Instant
