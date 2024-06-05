8216e4368acc7b70eee214e60945e556a3bacdab
kahverengi <ahmet@duruer.dev>
2024-06-05 23:36:19 +0300

    fix: fix mandatory

--- src/main/kotlin/co/statu/parsek/api/event/RouterEventListener.kt
+++ src/main/kotlin/co/statu/parsek/api/event/RouterEventListener.kt
@@ -5,7 +5,7 @@ import io.vertx.ext.web.Router
 
 interface RouterEventListener : ParsekEventListener {
 
-    fun onInitRouteList(routes: MutableList<Route>)
+    fun onInitRouteList(routes: MutableList<Route>) {}
 
-    fun onRouterCreate(router: Router)
+    fun onRouterCreate(router: Router) {}
 }
