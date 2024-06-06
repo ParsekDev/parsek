ebd60aa0693154c6186406a8456db99444b75c8e
kahverengi <ahmet@duruer.dev>
2024-06-06 10:54:07 +0300

    fix: rename and fix onBeforeCreateRoutes method

--- src/main/kotlin/co/statu/parsek/api/event/RouterEventListener.kt
+++ src/main/kotlin/co/statu/parsek/api/event/RouterEventListener.kt
@@ -1,10 +1,11 @@
 package co.statu.parsek.api.event
 
 import co.statu.parsek.model.Route
+import io.vertx.ext.web.Router
 
 interface RouterEventListener : ParsekEventListener {
 
     fun onInitRouteList(routes: MutableList<Route>) {}
 
-    fun onRouterCreate() {}
+    fun onBeforeCreateRoutes(router: Router) {}
 }
