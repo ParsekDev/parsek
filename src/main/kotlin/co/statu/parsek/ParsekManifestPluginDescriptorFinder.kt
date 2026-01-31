0aae5df216745a5c88a532bd5ca51405f5f429d0
kahverengi <ahmet@duruer.dev>
2026-01-31 19:41:41 +0300

    feat: upgraded spring context version to 5.3.39

--- src/main/kotlin/co/statu/parsek/ParsekManifestPluginDescriptorFinder.kt
+++ src/main/kotlin/co/statu/parsek/ParsekManifestPluginDescriptorFinder.kt
@@ -15,41 +15,9 @@ class ParsekManifestPluginDescriptorFinder : ManifestPluginDescriptorFinder() {
     }
 
     override fun createPluginDescriptor(manifest: Manifest): PluginDescriptor {
-        val pluginDescriptor = createPluginDescriptorInstance()
+        val pluginDescriptor = super.createPluginDescriptor(manifest) as ParsekPluginDescriptor
 
         val attributes = manifest.mainAttributes
-        val id = attributes.getValue(PLUGIN_ID)
-        pluginDescriptor.pluginId = id
-
-        val description = attributes.getValue(PLUGIN_DESCRIPTION)
-        pluginDescriptor.pluginDescription = if (StringUtils.isNullOrEmpty(description)) {
-            ""
-        } else {
-            description
-        }
-
-        val clazz = attributes.getValue(PLUGIN_CLASS)
-        if (StringUtils.isNotNullOrEmpty(clazz)) {
-            pluginDescriptor.setPluginClass(clazz)
-        }
-
-        val version = attributes.getValue(PLUGIN_VERSION)
-        if (StringUtils.isNotNullOrEmpty(version)) {
-            pluginDescriptor.setPluginVersion(version)
-        }
-
-        val provider = attributes.getValue(PLUGIN_PROVIDER)
-        pluginDescriptor.setProvider(provider)
-        val dependencies = attributes.getValue(PLUGIN_DEPENDENCIES)
-        pluginDescriptor.setDependencies(dependencies)
-
-        val requires = attributes.getValue(PLUGIN_REQUIRES)
-        if (StringUtils.isNotNullOrEmpty(requires)) {
-            pluginDescriptor.setRequires(requires)
-        }
-
-        pluginDescriptor.setLicense(attributes.getValue(PLUGIN_LICENSE))
-
         val sourceUrl = attributes.getValue(PLUGIN_SOURCE_URL)
         if (StringUtils.isNotNullOrEmpty(sourceUrl)) {
             pluginDescriptor.sourceUrl = sourceUrl
