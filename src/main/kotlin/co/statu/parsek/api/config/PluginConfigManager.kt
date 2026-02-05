ccdb02808278ecb02005b1009adbb8f86d7ce5c6
kahverengi <ahmet@duruer.dev>
2026-02-05 22:05:50 +0300

    fix: fixed plugin config migration not finishing migration

--- src/main/kotlin/co/statu/parsek/api/config/PluginConfigManager.kt
+++ src/main/kotlin/co/statu/parsek/api/config/PluginConfigManager.kt
@@ -103,10 +103,12 @@ class PluginConfigManager<T : PluginConfig>(
         updateConfig(JsonObject(config.root().unwrapped()))
     }
 
-    private fun migrate(configVersion: Int = config.version, saveConfig: Boolean = false) {
+    private fun migrate(
+        configAsJsonObject: JsonObject = JsonObject(gson.toJson(config)),
+        configVersion: Int = config.version,
+        saveConfig: Boolean = false
+    ) {
         logger.info("Checking available config migrations")
-
-        val configAsJsonObject = JsonObject(gson.toJson(config))
 
         migrations
             .find { configMigration -> configMigration.isMigratable(configVersion) }
@@ -117,7 +119,7 @@ class PluginConfigManager<T : PluginConfig>(
 
                 migration.migrate(configAsJsonObject)
 
-                migrate(migration.to, true)
+                migrate(configAsJsonObject, migration.to, true)
 
                 return
             }
